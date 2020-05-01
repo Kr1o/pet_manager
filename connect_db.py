28f5358ed1ef3241f293c5048e9aded551ae1eb8
localadmin <void.mamont@gmail.com>
2020-05-01 20:26:16 +0300

    tg bot

--- connect_db.py
+++ connect_db.py
@@ -1,8 +1,8 @@
 import os
 
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, Date
+from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 base_path = os.path.abspath('database.db')
 engine = create_engine(f'sqlite:///{base_path}', echo=True, encoding='utf-8')
@@ -10,22 +10,22 @@ Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
 
+pet_owner = Table('pet_owner', Base.metadata,
+                  Column('user_id', Integer, ForeignKey('user.id')),
+                  Column('pet_id', Integer, ForeignKey('pet.id'))
+                  )
+
 
 class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    birth_date = Column(Date)
-    feed_weight = Column(Integer)
-    feed_in_day = Column(Integer)
-    vaccination = Column(Boolean)
-    tick_protect = Column(Boolean)
-    vaccination_date = Column(Date)
-    tick_protect_date = Column(Date)
+    tg_user_id = Column(String)
+    tg_user_first_name = Column(String)
+    pets = relationship('Pet', secondary=pet_owner)
 
     def __repr__(self):
-        return "Имя: '%s', Дата рождения: '%s'" % (self.name, self.birth_date)
+        return "Имя: '%s', Телеграм ID: '%s'" % (self.tg_user_first_name, self.tg_user_id)
 
 
 class Pet(Base):
@@ -33,13 +33,14 @@ class Pet(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    birth_date = Column(Date)
-    feed_weight = Column(Integer)
-    feed_in_day = Column(Integer)
-    vaccination = Column(Boolean)
-    tick_protect = Column(Boolean)
-    vaccination_date = Column(Date)
-    tick_protect_date = Column(Date)
+
+    #    birth_date = Column(Date)
+    #    feed_weight = Column(Integer)
+    #    feed_in_day = Column(Integer)
+    #    vaccination = Column(Boolean)
+    #    tick_protect = Column(Boolean)
+    #    vaccination_date = Column(Date)
+    #    tick_protect_date = Column(Date)
 
     def __repr__(self):
-        return "Имя: '%s', Дата рождения: '%s'" % (self.name, self.birth_date)
+        return "Имя: '%s', Дата рождения: '%s'" % (self.name, self.name)
